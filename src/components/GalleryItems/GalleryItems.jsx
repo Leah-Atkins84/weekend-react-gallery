551aa05e4dcbfe145690de3fdf96f1620974885a
Leah Atkins <leah.atkins84@gmail.com>
2022-02-20 12:13:25 -0600

    Images loading, working on toggle

--- src/components/GalleryItems/GalleryItems.jsx
+++ src/components/GalleryItems/GalleryItems.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
+//import "../GalleryItems/GalleryItems.css";
 
 function GalleryItems ({id, imgSrc, description, likes, putLikes}) {
     console.log(id, imgSrc, description, likes);
    
-    const [toggle, setToggle] = useState(true);
     
-    const toggleItem = () => setToggle(!toggle);
 
     const handleClick = () =>{
         console.log('liked!!!' );
         putLikes(id);
       }
+
+    const [toggle, setToggle] = useState(true);
+    
+    const toggleItem = () => setToggle(!toggle);
+
    
     return(
         <>
-            
+           <div className = "gallery-items">
+               {toggle ? (
+                   <div className = "image-box">
+                       <img id={id} src={imgSrc} onClick={() => toggleItem()}/>
+                    </div>):
+                    (<div className="image-box" onClick={() => toggleItem()}>
+                            <p>{description}</p>
+                        </div>
+                    )
+               }
+               
+               </div> 
             <div>
                 <img src={imgSrc} alt={description}></img>
                 <button onClick={handleClick}>Like</button>

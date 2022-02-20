f7a916743013bc7f7735086e409e859706f19591
Leah Atkins <leah.atkins84@gmail.com>
2022-02-20 14:22:02 -0600

    more pics added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'https://msvivir.files.wordpress.com/2015/01/musicdance.jpg', description: 'Music and dancing is fun', likes: 0 },
+    { id: 1, path: 'https://classroomclipart.com/images/gallery/Clipart/Science/TN_girl-student-in-laboratory--performing-experiment-science-clipart.jpg', description: 'I used to work in a lab', likes: 0 },
+    { id: 2, path: 'https://cdn.w600.comps.canstockphoto.com/web-design-coding-vector-clipart_csp20539508.jpg', description: 'Now I do coding', likes: 0 },
     { id: 3, path: 'https://aspc.jobtrack.com.au/docs/web/st/help/resize/landscape.jpg?width=100', description: 'Nature is nice', likes: 0 },
     { id: 4, path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpMa7jvG4rxWBCu6Ew5ThQB_pmOlgCZRzlg&usqp=CAU', description: 'Still waiting for my invitation..', likes: 0 },
     { id: 5, path: 'https://cdn.wallpapersafari.com/69/37/loQIyb.jpg', description: 'Mermaids are awesome and they should exist', likes: 0 },
+    { id: 6, path: 'https://msvivir.files.wordpress.com/2015/01/musicdance.jpg', description: 'Music and dancing is fun', likes: 0 },
     
 ];
 
